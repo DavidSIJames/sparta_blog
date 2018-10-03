cd412545c658848bcd698d5eab3301b98e91659d
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-03 18:23:54 +0100

    added scss styling to the website

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,6 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments,
+  dependent: :destroy
   validates :title, :presence => true
   validates :body, :presence => true
   validates :body, length: {in: 1..240, message:"must be between 1-240 characters"}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :blogs
-  has_many :comments
+  has_many :comments,
+  dependent: :destroy
   validates :first_name, :presence => true
   validates :first_name, format: {with: /\A[a-zA-Z]+\z/, message:"must contain characters between a and z only"}
   validates :last_name, :presence => true
