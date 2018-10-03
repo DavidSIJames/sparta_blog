ff394bb2511c8d5265177fba06ea50a9a0af2279
Tech-A46 <tech-a46@Tech-a46s-MacBook-Air.local>
2018-10-03 17:39:59 +0100

    added more validations

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,6 @@ class Blog < ApplicationRecord
   has_many :comments
   validates :title, :presence => true
   validates :body, :presence => true
+  validates :body, length: {in: 1..240, message:"must be between 1-240 characters"}
   validates :image, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   has_many :blogs
   has_many :comments
   validates :first_name, :presence => true
+  validates :first_name, format: {with: /\A[a-zA-Z]+\z/, message:"must contain characters between a and z only"}
   validates :last_name, :presence => true
+  validates :last_name, format: {with: /\A[a-zA-Z]+\z/, message:"must contain characters between a and z only"}
   def full_name
     "#{first_name} #{last_name}"
   end
